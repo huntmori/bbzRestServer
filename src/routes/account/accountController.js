de8435113159b4c7b5aed2e08d0978dfb63e6805
huntmori04x22 <huntmori04x22@gmail.com>
2020-04-23 23:17:50 +0900

    test db conn

--- src/routes/account/accountController.js
+++ src/routes/account/accountController.js
@@ -1,6 +1,30 @@
-
+var mysql = require('mysql');
 
 exports.create = function(request, response)
 {
-    response.end('controller.create');
+    var msg = '';
+
+    var connection = mysql.createConnection({
+         host : 'localhost'
+        ,port : 3307
+        ,user : 'root'
+        ,password : ''
+        ,database : 'bbz_db'
+    });
+    connection.connect();
+    connection.query("SELECT now() ", function(error, rows, fields)
+    {
+        connection.end();
+        if(!error)
+        {
+            console.log(rows);
+            console.log(fields);
+
+            msg = 'rows:'+JSON.stringify(rows);
+            console.log(msg);
+            response.send(msg);
+        }
+    });
+
+    response.send(msg);
 }
