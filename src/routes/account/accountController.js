9b8d94d6a1ae7a05cbb0fb284e15dfaed1317801
huntmori04x22 <huntmori04x22@gmail.com>
2020-05-04 00:02:40 +0900

    test request queryparams

--- src/routes/account/accountController.js
+++ src/routes/account/accountController.js
@@ -5,6 +5,10 @@ exports.create = function(request, response)
 {
     var msg = '';
 
+    var params = request.params;
+    console.log("params", params);
+    console.log("query", request.query);
+
     var connection = mysql.createConnection({
          host : 'localhost'
         ,port : 3307
@@ -39,17 +43,23 @@ exports.test = function(request, response)
     console.log("test");
     var pool = mysql.createPool(db);
 
-    pool.query("SELECT now() ", function(error, rows, fields)
+    pool.query("SELECT now() as date", function(error, rows, fields)
     {
         //pool.releaseConnection();
         if(!error)
         {
+            let result = new Object();
+            result.data = Array();
+            for(let i=0; i< rows.length; i++){
+                console.log(i+":"+rows[i]);
+                result.data.push(rows[i]);
+            }
             console.log(rows);
             console.log(fields);
             console.log(rows.length);
             msg = JSON.stringify(rows);
             console.log(msg);
-            response.json(msg);
+            response.json(result);
         }
         else
         {
