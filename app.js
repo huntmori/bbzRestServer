e44e956b9707e18a8d18aa1e85df37a0cdeed2a7
huntmori04x22 <huntmori04x22@gmail.com>
2020-05-08 00:21:58 +0900

    Request body test

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const   express = require('express');
 const   app     = express();
 const   http    = require('http').createServer(app);
 const   port    = 8001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 //const   dbConfig = require('./src/config/dbConfig');
 const   accountRoute = require("./src/routes/account/accountIndex");
+
+
 app.use (accountRoute);
+
+
 
 http.listen(port, function()
 {
