7c161c8c294aba5dcd25f8f0b33cd8db4ec7e210
huntmori04x22 <huntmori04x22@gmail.com>
2020-05-25 00:24:24 +0900

    resize client unityContainer

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const   app     = express();
 const   http    = require('http').createServer(app);
 const   port    = 8080;
 const fs = require("fs");
-/*const moment = require('moment');
-require('moment-timezone');*/
-//app.use(express.static(__dirname+"/view"));
-//moment.tz.setDefault('Asia/Seoul');
-/*app.use(express.json());
-app.use(express.urlencoded({extended:true}));*/
-//const   dbConfig = require('./src/config/dbConfig');
+const moment = require('moment');
+require('moment-timezone');
+app.use(express.static(__dirname+"/view"));
+moment.tz.setDefault('Asia/Seoul');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+const   dbConfig = require('./src/config/dbConfig');
 const   accountRoute = require("./src/routes/account/accountIndex");
 
 
@@ -19,10 +19,10 @@ app.use("/TemplateData", express.static(__dirname+"/TemplateData"));
 app.use("/Build", express.static(__dirname+"/Build"));
 app.get("/", function(request, response){
     var data = fs.readFileSync(__dirname+"/index.html",'utf-8');
-    //console.log(data);
+    
     const ip = request.headers['x-forwarded-for'] ||  request.connection.remoteAddress;
-    //const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
-    //console.log("["+timestamp+"] access from : "+ip);
+    const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+    console.log("["+timestamp+"] access from : "+ip);
     response.end(data);
 });
 
